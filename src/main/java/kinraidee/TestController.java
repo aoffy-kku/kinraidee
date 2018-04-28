db445c9e02183b447a7b43b7acc527e4925d32db
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-04-28 21:10:24 +0700

    Create spring framework project

---
+++ src/main/java/kinraidee/TestController.java
@@ -0,0 +1,16 @@
+package kinraidee;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, World";
+	}
+	
+}
