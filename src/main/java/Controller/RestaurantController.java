3353c88da1deefaacbeda200df873ed57665284f
Noina <noina.17@hotmail.com>
2018-05-02 03:20:29 +0700

    Create restaurant

---
+++ src/main/java/Controller/RestaurantController.java
@@ -0,0 +1,70 @@
+package Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import Model.Comment;
+import Model.Restaurant;
+import Model.RestaurantRepository;
+import Model.User;
+
+@RestController
+public class RestaurantController {
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+
+	@GetMapping("/restaurant") // show all restaurant
+	public List<Restaurant> getRestaurantList() {
+		List<Restaurant> restaurantList = restaurantRepository.findAllRestaurant();
+		return restaurantList;
+	}
+
+	@PostMapping("/restaurant") // insertRestaurant
+	public Restaurant createRestaurant(@RequestBody Restaurant restaurant) {
+		return restaurantRepository.insertRestaurant(restaurant);
+	}
+
+	@GetMapping("/restaurant/{resId}") // search restaurant by id
+	public Restaurant getRestaurantById(@PathVariable Integer resId) {
+		Restaurant restaurant = restaurantRepository.findRestaurantById(resId);
+		return restaurant;
+	}
+
+	@PutMapping("/restaurant/{resId}") // update restaurant information
+	public Restaurant updateRestaurant(@PathVariable Integer resId,@RequestBody Restaurant restaurant) {
+		Restaurant oldRestaurant = restaurantRepository.findRestaurantById(resId);	
+		oldRestaurant.setResId(restaurant.getResId());
+		oldRestaurant.setUserId(restaurant.getUserId());
+		oldRestaurant.setRestaurantName(restaurant.getRestaurantName());
+		oldRestaurant.setDescription(restaurant.getDescription());
+		oldRestaurant.setType(restaurant.getDescription());
+		oldRestaurant.setVegan(restaurant.isVegan());
+		oldRestaurant.setWifi(restaurant.isWifi());
+		oldRestaurant.setPlug(restaurant.isPlug());
+		oldRestaurant.setAir(restaurant.isAir());
+		oldRestaurant.setMotorcycle(restaurant.isMotorcycle());
+		oldRestaurant.setCar(restaurant.isCar());
+		oldRestaurant.setOpen(restaurant.isOpen());	
+		oldRestaurant.setLat(restaurant.getLat());
+		oldRestaurant.setLng(restaurant.getLng());
+		oldRestaurant.setLogo(restaurant.getLogo());
+		oldRestaurant.setCover(restaurant.getCover());
+		oldRestaurant.setCreateAt(restaurant.getCreateAt());
+		oldRestaurant.setUpdateAt(restaurant.getUpdateAt());
+		Restaurant newRestaurant = restaurantRepository.insertRestaurant(oldRestaurant);
+		return newRestaurant;
+	}
+	
+	@DeleteMapping("/restaurant/{resId}")
+	public void deleteRestaurant(@PathVariable Integer resId) {
+		restaurantRepository.deleteRestaurant(resId);
+	}
+}
---
+++ src/main/java/Model/RestaurantRepository.java
@@ -0,0 +1,34 @@
+package Model;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RestaurantRepository {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public List<Restaurant> findAllRestaurant() {
+		Query query = entityManager.createQuery("from restaurant");
+		return query.getResultList();
+	}
+
+	public Restaurant findRestaurantById(Integer resId) {
+		return entityManager.find(Restaurant.class, resId);
+	}
+	
+	@Transactional
+	public Restaurant insertRestaurant(Restaurant restaurant) {
+	entityManager.persist(restaurant);
+	return restaurant;
+	}
+	
+	@Transactional
+	public void deleteRestaurant(Integer resId) {
+	Restaurant restaurant = entityManager.find(Restaurant.class, resId);
+	entityManager.remove(restaurant);
+	}
+}
