9f37701fed8e3247926afd3fa62c1e514f0150e2
ChutikanS <chutikan.suphen@gmail.com>
2018-05-02 21:13:29 +0700

    edit comment

--- src/main/java/kinraidee/controller/CommentController.java
+++ src/main/java/kinraidee/controller/CommentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,22 +19,31 @@ public class CommentController {
 	@Autowired
 	private CommentRepository commentRepository;
 	
+	//insert comment
+		@PostMapping("/comment")
+		public Comment createComment(@RequestBody Comment comment) {
+			return commentRepository.insert(comment);
+		}
+	
 	//Show all comment
 	@GetMapping("/comment")
 	public List<Comment> getCommentList(){
-		List<Comment> commentList = commentRepository.findAllComment();
+		List<Comment> commentList = commentRepository.findAll();
 		return commentList;
 	}
 	
-	//insert comment
-	@PostMapping("/comment")
-	public Comment createComment(@RequestBody Comment comment) {
-		return commentRepository.insertComment(comment);
+	//search by comment_id
+	@PutMapping("/comment/{comment_id}")
+	public Comment updateComment(@PathVariable Integer comment_id, @RequestBody Comment comment) {
+		Comment newComment = commentRepository.update(comment_id, comment);
+		return newComment;
 	}
 	
 	//delete comment
 	@DeleteMapping("/comment/{comment_id}")
 	public void deleteComment(@PathVariable Integer comment_id) {
-		commentRepository.deleteComment(comment_id);
+		commentRepository.delete(comment_id);
 	}
+	
+	
 }
