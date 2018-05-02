68d3c5be2b4344b70f437ed7636714d88229f9cd
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-05-03 01:45:39 +0700

    Add cross-origin

--- src/main/java/kinraidee/controller/UserController.java
+++ src/main/java/kinraidee/controller/UserController.java
@@ -3,17 +3,20 @@ package kinraidee.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import kinraidee.model.User;
 import kinraidee.model.UserRepository;
-
+@CrossOrigin(origins = "*" ,maxAge = 3600)
 @RestController
 public class UserController {
 	@Autowired
